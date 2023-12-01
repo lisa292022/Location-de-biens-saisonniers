8c35d22c3d060dda4835fd03faf9fda9f741a8d7
Lisa DEFEUILLET <lisa.defeuillet@gmail.com>
2023-12-01 19:05:29 +0100

    CRUD bien

--- biens/biens_traitement.php
+++ biens/biens_traitement.php
@@ -10,20 +10,11 @@
     $ocommune = new Commune(1);
     $ocommune->setcode($code);
         
-    if(isset($_POST['ajouter2'])){
-        echo "<script>alert('Etes-vous certain de vouloir ajouter ?');</script>;";
-        // récupère id de type_bien
-        $MonTypebienId=$oTypebien->getId_type_bien($_POST['lib_type_bien']);
-        $idcom = $ocommune->getidcom($_POST['cop_bien'],$_POST['vil_bien']);
-        $oBiens->insertBien($_POST['nom_bien'],$_POST['rue_bien'],$idcom,$_POST['superficie_bien'], $_POST['nb_couchage'], $_POST['nb_piece'], $_POST['nb_chambre'], $_POST['descriptif'], $_POST['ref_bien'], $_POST['statut_bien'], $_POST['lib_type_bien']);
-        header('Location: biens_affichage.php');
-    }
     if(isset($_POST['ajouter'])){
         echo "<script>alert('Etes-vous certain de vouloir ajouter ?');</script>;";
         // récupère id de type_bien
         echo $_POST['lib_type_bien'][0];
         echo $_POST['lib_type_bien'];
-        //$MonTypebienId=$oTypebien->getIdTypeBien($_POST['lib_type_bien'][0]);
         $MonTypebienId=$_POST['lib_type_bien'];
         $idcom = $ocommune->getidcomCouple($_POST['cop_vil_bien']);
         $oBiens->insertBien($_POST['nom_bien'],$_POST['rue_bien'],$idcom,$_POST['superficie_bien'], $_POST['nb_couchage'], $_POST['nb_piece'], $_POST['nb_chambre'], $_POST['descriptif'], $_POST['ref_bien'], $_POST['statut_bien'], $MonTypebienId);
@@ -35,14 +26,6 @@
         $oBiens->deleteBien($id_bien);
         header('Location: biens_affichage.php');
     }
-    if(isset($_POST['modifier2'])){
-        echo "<script>alert('Etes-vous certain de vouloir modifier ?');</script>";
-        $id_bien = $_POST['modifier'];
-        $idcom = $ocommune->getidcom($_POST['cop_bien'.$id_bien],$_POST['vil_bien'.$id_bien]);
-        $MonTypebienId=$oTypebien->getId_type_bien($_POST['lib_type_bien']);
-        $oBiens->updateBien($id_bien,$_POST['nom_bien'.$id_bien],$_POST['rue_bien'.$id_bien],$idcom,$_POST['superficie_bien'.$id_bien],$_POST['nb_couchage'.$id_bien],$_POST['nb_piece'.$id_bien],$_POST['nb_chambre'.$id_bien],$_POST['descriptif'.$id_bien],$_POST['ref_bien'.$id_bien],$_POST['statut_bien'.$id_bien], $_POST['lib_type_bien']);
-        header('Location: biens_affichage.php');
-    }
     if(isset($_POST['modifier'])){
         echo "<script>alert('Etes-vous certain de vouloir modifier ?');</script>";
         $id_bien = $_POST['modifier'];
@@ -50,6 +33,5 @@
         $MonTypebienId=$oTypebien->getIdTypeBien($_POST['lib_type_bien'.$id_bien]);
         $oBiens->updateBien($id_bien,$_POST['nom_bien'.$id_bien],$_POST['rue_bien'.$id_bien],$idcom,$_POST['superficie_bien'.$id_bien],$_POST['nb_couchage'.$id_bien],$_POST['nb_piece'.$id_bien],$_POST['nb_chambre'.$id_bien],$_POST['descriptif'.$id_bien],$_POST['ref_bien'.$id_bien],$_POST['statut_bien'.$id_bien], $MonTypebienId);
         header('Location: biens_affichage.php');
-    }
-       
+    }      
 ?>
